0ef02b8c615e2e500b120cf19687b43ecb247666
Lucas Palhano Fonseca <lucas_palhano_@hotmail.com>
2023-04-01 17:37:36 -0300

    Adição de um novo artigo

--- src/pages/api/posts/data.ts
+++ src/pages/api/posts/data.ts
@@ -1,48 +1,61 @@
 export const posts = [
-    {
-        id: 5,
-        url: "https://medium.com/@lucaspalhanof/cria%C3%A7%C3%A3o-de-ascii-art-usando-python-7c4724491a9?source=rss-6a633f9d0fd7------2",
-        img: "https://cdn-images-1.medium.com/max/1024/1*_yybdN1rMCKmweos2r-Z2A.png",
-        date: "02/03/2023",
-        title: "Criação de ASCII Art usando Python",
-        description: `Eu sempre achei muito interessante aquelas ilustrações feitas em terminais de computadores
+  {
+    id: 6,
+    url: "https://medium.com/@lucaspalhanof/suas-senhas-s%C3%A3o-muito-provavelmente-ruins-parte-1-299abc1d46a7",
+    img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*M2HbpKKa3AT1EHVoc_4iXg.png",
+    date: "28/03/2023",
+    title: "Suas senhas são (muito provavelmente) ruins — Parte 1",
+    description: `Senhas. Todos os dias, em algum momento, temos que lidar com elas. Seja para acessar nossas contas
+         bancárias, redes sociais, e-mails ou mesmo um jogo para passar o tempo, elas estão sempre lá, no meio do caminho. 
+         E parece que elas nunca são boas o bastante: a senha é muito curta, muito longa, não tem caracteres especiais 
+         suficientes, é fácil demais de adivinhar, é difícil demais para lembrar… É como se as senhas existissem apenas 
+         para dificultar as nossas vidas.`,
+    tags: ["Senhas", "Hacking", "Ciberseurança"],
+  },
+  {
+    id: 5,
+    url: "https://medium.com/@lucaspalhanof/cria%C3%A7%C3%A3o-de-ascii-art-usando-python-7c4724491a9?source=rss-6a633f9d0fd7------2",
+    img: "https://cdn-images-1.medium.com/max/1024/1*_yybdN1rMCKmweos2r-Z2A.png",
+    date: "02/03/2023",
+    title: "Criação de ASCII Art usando Python",
+    description: `Eu sempre achei muito interessante aquelas ilustrações feitas em terminais de computadores
          usando apenas dígitos do teclado, ao clássico estilo Matrix. Este estilo de desenho digital é chamado de ASCII Art, 
          e pesquisando um pouco sobre este assunto eu percebi que seria possível desenvolver um algoritmo de geração 
          de imagens e vídeos neste estilo.`,
-        tags: ["Python", "Ascii", "OpenCV", "Imagem"],
-    },
-    {
-        id: 4,
-        url: "https://medium.com/@lucaspalhanof/o-zen-do-java-e-a-developer-experience-dx-847b3d787699?source=rss-6a633f9d0fd7------2",
-        img: "https://cdn-images-1.medium.com/max/1024/1*7RnsdscHeMix46awfd-vtA.png",
-        date: "13/02/2023",
-        title: "O Zen do Java e a Developer Experience (DX)",
-        description: `Muitas vezes nós desenvolvedores ouvimos falar sobre UX (User Experience), 
+    tags: ["Python", "Ascii", "OpenCV", "Imagem"],
+  },
+  {
+    id: 4,
+    url: "https://medium.com/@lucaspalhanof/o-zen-do-java-e-a-developer-experience-dx-847b3d787699?source=rss-6a633f9d0fd7------2",
+    img: "https://cdn-images-1.medium.com/max/1024/1*7RnsdscHeMix46awfd-vtA.png",
+    date: "13/02/2023",
+    title: "O Zen do Java e a Developer Experience (DX)",
+    description: `Muitas vezes nós desenvolvedores ouvimos falar sobre UX (User Experience), 
         e nos preocupamos em desenvolver sistema amigáveis e intuitivos para os usuários, uma vez 
         que sistemas devem ser desenvolvidos para resolver problemas, e não criá-los. 
         Contudo, em muitas ocasiões pensamos pouco sobre a qualidade do nosso ambiente de desenvolvimento, 
         assim como a facilidade ou dificuldade de codificar com uma determinada linguagem, framework ou arquitetura de projeto.`,
-        tags: ["Java", "Python"],
-    },
-    {
-        id: 3,
-        url: "https://medium.com/@lucaspalhanof/buscando-um-bom-algoritmo-de-buscas-1ae8bd0ef04b?source=rss-6a633f9d0fd7------2",
-        img: "https://cdn-images-1.medium.com/max/1000/0*ZFz5UCEjjBz7Qg8k",
-        date: "28/01/2023",
-        title: "Buscando um bom algoritmo de buscas",
-        description: `No dia a dia é normal não conseguirmos encontrar coisas que precisamos, 
+    tags: ["Java", "Python"],
+  },
+  {
+    id: 3,
+    url: "https://medium.com/@lucaspalhanof/buscando-um-bom-algoritmo-de-buscas-1ae8bd0ef04b?source=rss-6a633f9d0fd7------2",
+    img: "https://cdn-images-1.medium.com/max/1000/0*ZFz5UCEjjBz7Qg8k",
+    date: "28/01/2023",
+    title: "Buscando um bom algoritmo de buscas",
+    description: `No dia a dia é normal não conseguirmos encontrar coisas que precisamos, 
         como o controle da TV que está na nossa mão ou os óculos que estão justamente onde deveriam estar, 
         ou seja, na frente dos nossos olhos. Isso é normal, e apesar do inconveniente momentâneo, 
         não gera grandes transtornos e às vezes até rende boas risadas.`,
-        tags: ["Python", "Algoritmo", "Busca"],
-    },
-    {
-        id: 2,
-        url: "https://medium.com/@lucaspalhanof/o-inevit%C3%A1vel-v%C3%ADcio-em-intelig%C3%AAncias-artificiais-24e5532d0d59?source=rss-6a633f9d0fd7------2",
-        img: "https://cdn-images-1.medium.com/max/500/0*-xex7RYPY0ACVOeV.gif",
-        date: "12/01/2023",
-        title: "O inevitável vício em Inteligências Artificiais",
-        description: `[…] Eu me tornei vítima do vício em Inteligência Artificial. 
+    tags: ["Python", "Algoritmo", "Busca"],
+  },
+  {
+    id: 2,
+    url: "https://medium.com/@lucaspalhanof/o-inevit%C3%A1vel-v%C3%ADcio-em-intelig%C3%AAncias-artificiais-24e5532d0d59?source=rss-6a633f9d0fd7------2",
+    img: "https://cdn-images-1.medium.com/max/500/0*-xex7RYPY0ACVOeV.gif",
+    date: "12/01/2023",
+    title: "O inevitável vício em Inteligências Artificiais",
+    description: `[…] Eu me tornei vítima do vício em Inteligência Artificial. 
         Percebi que estou usando a AI para realizar todos os tipos de tarefas, 
         desde as mais complexas até as mais simples, e isso tem prejudicado minha 
         capacidade de tomar decisões e raciocinar por conta própria. Eu me vejo 
@@ -51,19 +64,19 @@ export const posts = [
         vício está me impedindo de alcançar meu pleno potencial e de viver uma 
         vida autônoma e independente. Por isso, é necessário lutar para que minha 
         vida seja plena novamente.`,
-        tags: ["IA", "Futuro", "Tecnologia"],
-    },
-    {
-        id: 1,
-        url: "https://medium.com/@lucaspalhanof/script-do-dia-perdendo-na-mega-da-virada-com-estilo-usando-python-5828b081b64d?source=rss-6a633f9d0fd7------2",
-        img: "https://cdn-images-1.medium.com/max/700/1*jNyIXzCrMC7gxQARFaFnPA.jpeg",
-        date: "31/12/2022",
-        title: "Perdendo na Mega da Virada com estilo usando Python",
-        description: `Quem quer ser um meio bilionário? Não olhem para mim, 
+    tags: ["IA", "Futuro", "Tecnologia"],
+  },
+  {
+    id: 1,
+    url: "https://medium.com/@lucaspalhanof/script-do-dia-perdendo-na-mega-da-virada-com-estilo-usando-python-5828b081b64d?source=rss-6a633f9d0fd7------2",
+    img: "https://cdn-images-1.medium.com/max/700/1*jNyIXzCrMC7gxQARFaFnPA.jpeg",
+    date: "31/12/2022",
+    title: "Perdendo na Mega da Virada com estilo usando Python",
+    description: `Quem quer ser um meio bilionário? Não olhem para mim, 
         eu preferiria ser um bilionário por completo! Brincadeiras à parte, 
         a Mega da Virada está com um prêmio de R$ 500.000.000,00 o que pode 
         ser lido como meio bilhão de reais, ou a primeira parcela do iPhone 
         15 que será lançado em 2023.`,
-        tags: ["Python"],
-    },
-]
+    tags: ["Python"],
+  },
+];

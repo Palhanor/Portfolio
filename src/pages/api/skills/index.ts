5d56df0ac6b88fae47d0d16ef6882a603116a679
Lucas Palhano <palhano@Lucas-MacBook-Air.local>
2026-02-12 23:39:09 -0300

    Fix the structure of the API in NextJS

--- src/pages/api/skills/index.ts
+++ src/pages/api/skills/index.ts
@@ -1,5 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { skills } from "./data";
+
+export const skills = [
+    {
+        title: "Linguagens",
+        value: "linguagens",
+        items: [
+            "HTML5", "CSS3", "JavaScript", "TypeScript", "Python"
+        ]
+    },
+    {
+        title: "Bibliotecas e Frameworks",
+        value: "frameworks",
+        items: [
+            "SASS", "React", "React Native", "Bootstrap", "TailwindCSS", "NextJS", "Expo"
+        ]
+    },
+    {
+        title: "Ferramentas",
+        value: "ferramentas",
+        items: [
+            "GIT", "GitHub", "VS Code", "Markdown", "PyCharm"
+        ]
+    },
+];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json(skills);
